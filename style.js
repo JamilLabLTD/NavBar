d85e011ac26bf191ee8506b4bb77a8eb9fcd9f94
Lutfur Rahman Jamil <93875782+JamilLabLTD@users.noreply.github.com>
2022-01-12 00:46:47 +0600

    Nav Bar Animation

---
+++ style.js
@@ -0,0 +1,23 @@
+/*---Don't Try to edit this Part----*/
+/*Nav Bar Animation*/
+function CloseNaveSidesLeftTo() {
+  document.getElementById("nav_right_area").style.marginRight = "-20%";
+  x =  document.getElementById("nav-left_area");
+  x.style.marginLeft = "-80%";
+  }
+function CloseNaveSidesRightTo() {
+  document.getElementById("nav_right_area").style.marginRight = "-20%";
+    x =  document.getElementById("nav-left_area");
+  x.style.marginLeft = "-80%";
+  }
+function MainHeaderToShow(){
+  document.getElementById("nav_right_area").style.marginRight = "0%";
+  x =  document.getElementById("nav-left_area");
+  x.style.marginLeft = "0%";
+}
+function NavRightArea(){
+  x =  document.getElementById("nav-left_area");
+  x.style.marginLeft = "-80%";
+  document.getElementById("nav_right_area").style.marginRight = "-20%";
+}
+/*--------***----------*/
